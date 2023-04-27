0cd191e3c2b11e4ffd74230d24df30d66d6c4e72
Class01 <Class01@DESKTOP-NAEGOQB>
2023-04-27 18:09:11 +0900

    0427

---
+++ jungol/src/q108/Main.java
@@ -0,0 +1,14 @@
+package q108;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a= 50;
+		double b = 100.12;
+		System.out.printf("%.2f * %d = %.0f",b,a,b*a);
+		
+
+	}
+
+}
